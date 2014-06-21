fa87a9624499a9083573d621e14d0d46df8f1b23
cicee <cicee@Tao-VAIO>
2014-06-21 22:19:54 +0900

    first l2r

---
+++ RankLib/src/org/archive/IDirectory.java
@@ -0,0 +1,10 @@
+package org.archive;
+
+public class IDirectory {
+	//
+	public static final String ROOT_OUTPUT = "H:/v-haiyu/CodeBench/Pool_Output/Output_Ranklib/";
+	
+	//
+	public static final String ROOT_DATASET = "H:/v-haiyu/CodeBench/Pool_DataSet/DataSet_Ranklib/";
+
+}
